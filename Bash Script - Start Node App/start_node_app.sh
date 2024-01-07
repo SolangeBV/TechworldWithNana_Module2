10ab4e6016e4129d8394c28ac0b33d76409bf290
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2024-01-07 13:34:23 +0100

    Create start_node_app.sh

---
+++ Bash Script - Start Node App/start_node_app.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Function to display an error message and exit the script
+display_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+# Install Node.js and NPM and print out versions
+echo "Installing Node.js and NPM..."
+sudo apt update || display_error "Failed to update package list"
+sudo apt install -y nodejs npm || display_error "Failed to install Node.js and NPM"
+node_version=$(node -v)
+npm_version=$(npm -v)
+echo "Node.js version installed: $node_version"
+echo "NPM version installed: $npm_version"
+
+# Download artifact file
+echo "Downloading artifact file..."
+artifact_url="https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz"
+wget -q "$artifact_url" || display_error "Failed to download artifact file"
+
+# Unzip downloaded file
+echo "Unzipping downloaded file..."
+tar -xf bootcamp-node-envvars-project-1.0.0.tgz || display_error "Failed to unzip downloaded file"
+
+# Set environment variables
+export APP_ENV=dev
+export DB_USER=myuser
+export DB_PWD=mysecret
+
+# Change into the unzipped package directory
+cd bootcamp-node-envvars-project-1.0.0 || display_error "Failed to change directory"
+
+# Run NodeJS application
+echo "Running NodeJS application in background..."
+npm install || display_error "Failed to install Node.js dependencies"
+node server.js &
+
+echo "NodeJS application started successfully!"
